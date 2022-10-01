843bc9db76d3953dc7ab90240754d6af2778c860
Avadhoot Kulkarni <avadhootkulkarni06@gmail.com>
2022-10-01 15:10:28 +0530

    Added Hello World program in Python

--- scripts/Hello_World_New.py
+++ scripts/Hello_World_New.py
@@ -1,4 +1,5 @@
-// LANGUAGE: Python
-// AUTHOR: Avadhoot Kulkarni
-// GITHUB: https://github.com/AvadhootK
+# LANGUAGE: Python
+# AUTHOR: Avadhoot Kulkarni
+# GITHUB: https://github.com/AvadhootK
+
 print("Hello World!")
