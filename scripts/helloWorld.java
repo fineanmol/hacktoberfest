6fd2ac3fd3f8bbac711d284e4cf8301e05a92f4d
Aastha Kumari <aasthak108@gmail.com>
2022-10-01 15:01:20 +0530

    update

--- scripts/helloWorld.java
+++ scripts/helloWorld.java
@@ -1,5 +1,8 @@
-public class helloWorld {
-  public static void main(String[] args){
-    System.out.println("Hello World");
-  }
+
+class HelloWorld
+{
+	public static void main (String[]args)
+	{
+		System.out.println("Hello -> from Aastha, NMIT");
+	}
 }
