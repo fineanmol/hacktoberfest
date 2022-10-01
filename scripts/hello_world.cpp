d18710710bb40652b6625b06bbef584e0b4d4c34
Vedant Sharma <vedant.12sharma@gmail.com>
2022-10-01 14:18:04 +0530

    updated hello world in C

--- scripts/hello_world.cpp
+++ scripts/hello_world.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-
-int main() {
-    std::ios_base::sync_with_stdio(false);
-
-    std::cout << "Hello, World" << std::endl;
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello world";
     return 0;
 }
