dd96b28b41338222e50f1c83b22b035a7314e43d
Rohan Raju Meshram <meshramrohan786@gmail.com>
2022-10-02 01:06:27 +0530

    Updated Scripts

---
+++ scripts/hello_world_RohanKGP.cpp
@@ -0,0 +1,15 @@
+/*
+ LANGUAGE: C++
+ Author: Rohan Raju Meshram
+ Github: https://github.com/RohanKGP
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello, World!" <<endl;
+    return 0;
+}
+
