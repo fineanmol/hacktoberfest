4f9238b7929a21f0209037862d6c7946961eb7e2
PunGrumpy <noppakorn.pun@gmail.com>
2022-10-01 19:32:40 +0700

    add helloWorldPunGrumpy.ts

---
+++ scripts/helloWorldPunGrumpy.ts
@@ -0,0 +1,8 @@
+// Language: typescript
+// Path: scripts\helloWorldPunGrumpy.ts
+const greeting = ( message: string ) => {
+    console.log(`Hello ${message}`);
+}
+
+greeting('World'); // Hello World
+
