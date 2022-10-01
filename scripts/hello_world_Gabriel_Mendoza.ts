0945005af14f35429a640168e83501b6efc7fb24
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-10-01 15:06:53 +0800

    Add hello world Gabriel Mendoza

---
+++ scripts/hello_world_Gabriel_Mendoza.ts
@@ -0,0 +1,9 @@
+// LANGUAGE: Typescript
+//  ENV: Node.js
+//  AUTHOR: Gabriel Mendoza
+//  GITHUB: https://github.com/GabTheGreat25
+//
+
+let person: string = "(Made By: Gabriel Mendoza)";
+let message: string = "Hello, World! " + person;
+console.log(message);
