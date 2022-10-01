1720f3546f5ed71b6b2d479ea7213c758b3f1faf
Muhammad A. Noorani <muhammadnoorani112@gmail.com>
2022-10-01 16:37:38 +0530

    added-script

---
+++ scripts/MuhammadNoorani.py
@@ -0,0 +1,13 @@
+'''
+     LANGUAGE: Python
+     ENV: Python3.5
+     AUTHOR: MuhammadNoorani
+     GITHUB: https://github.com/MuhammadNoorani
+'''
+# This is how we create a function in python
+def PythonFirstProgram():
+	print('In python there are no curly braces, only indentation')
+# Indentartion is used to specify a block of code
+
+#Calling function
+PythonFirstProgram()
