f7d98d9c7c0881b2231ecc4a979470b998dd2583
Varshil Patel <GMCA09@gmcaman.gujgov.edu.in>
2022-10-02 08:49:32 +0100

    Create HelloWorld.sh

---
+++ HelloWorld.sh
@@ -0,0 +1,17 @@
+Create a new file helloworld.sh (using any text editor):
+
+nano helloworld.sh
+
+Write the following code:
+#!/bin/bash
+echo "Hello World"
+
+add permission by giving the command given below:
+chmod +x helloworld.sh
+
+Run using:
+./helloworld.sh
+or
+bash helloworld.sh
+or
+sh helloworld.sh
