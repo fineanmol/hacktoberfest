25c589147df4a72f2883967a5e653282e66e2b4c
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2022-10-02 20:49:06 -0400

    'Enhancing'

--- scripts/hello_world_beatriz-sabino.py
+++ scripts/hello_world_beatriz-sabino.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+
+# Changing the above code to class based design.
+
+class Greetings():
+    def __init__(self, greetingType) -> None:
+        self.type = greetingType
+
+    def testing(self):
+        if self.type == 'General':
+            return "Hello World"
+        elif self.type == "Morning":
+            return "Good Morning world!"
+        elif self.type == "Evening":
+            return "Good Evening World"
+        else:
+            return "Hello!"
+
+
+
+user1 = Greetings("Morning")
+print(user1.testing())
