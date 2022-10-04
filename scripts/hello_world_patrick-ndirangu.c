258a4676bf0b994ccfd7c5d2f1206916415af88d
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-10-04 17:46:01 +0300

    Added Hello world C file

---
+++ scripts/hello_world_patrick-ndirangu.c
@@ -0,0 +1,13 @@
+// LANGUAGE: C
+// ENV: GCC
+// AUTHOR: Patrick Ndirangu
+// GITHUB: https://github.com/patrick-ndirangu
+
+#include <stdio.h>
+
+// Displaying Hello World
+int main()
+{
+    printf("Hello, World!");
+    return (0);
+}
