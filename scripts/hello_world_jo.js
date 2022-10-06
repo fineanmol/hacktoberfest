a791651f65fd36c16133c4e75ec54e6fb5bc565e
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-10-05 20:07:15 -0400

    Hacktoberfest contribution
    Getting into open source projects!

---
+++ scripts/hello_world_jo.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Oluwatomilola
+// GITHUB: https://github.com/JO-DEV-IL
+
+function sayHello(yournamehere){
+    console.log(`Hello World! Hello ${yournamehere}!`)
+}
+
+sayHello()
