1562e34e1a77e5a2fb5a68acd42e7b72196d7aba
dojetin <102966393+dojetin@users.noreply.github.com>
2022-10-06 08:51:47 +0200

    Add files via upload

---
+++ scripts/Hello_Hub_dojetin.java
@@ -0,0 +1,10 @@
+package holaMundo;
+
+public class Hello_Hub_dojetin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
