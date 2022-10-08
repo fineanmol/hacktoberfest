e3f49e9332398afafa08d3009b0a17e3059529ab
Pramuditha Karunarathna <shemil076@gmail.com>
2022-10-08 12:25:43 +0530

    Added a hello world script

---
+++ scripts/hello_world_shemil076.ts
@@ -0,0 +1,7 @@
+// LANGUAGE: TypeScript
+// ENV: Node.js
+// AUTHOR: shemil076
+// GITHUB: https://github.com/shemil076
+console.log("================================")
+console.log("==>> Hello world");
+console.log("================================")
