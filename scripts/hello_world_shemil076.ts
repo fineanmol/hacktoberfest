f5943e749091b9acc3618c6b5aed90ed8cf1b185
Pramuditha Karunarathna <shemil076@gmail.com>
2022-10-08 12:33:28 +0530

    Changed the file

--- scripts/hello_world_shemil076.ts
+++
@@ -1,7 +0,0 @@
-// LANGUAGE: TypeScript
-// ENV: Node.js
-// AUTHOR: shemil076
-// GITHUB: https://github.com/shemil076
-console.log("================================")
-console.log("==>> Hello world");
-console.log("================================")
