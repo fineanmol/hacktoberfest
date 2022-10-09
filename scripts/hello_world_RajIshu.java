7932f4850a346b5cc730ee6f79b50ba0fbb21d1f
Ishu Raj <gokuishu57@gmail.com>
2022-10-10 03:05:31 +0530

    Update hello_world_RajIshu.java

--- scripts/hello_world_RajIshu.java
+++ scripts/hello_world_RajIshu.java
@@ -1,3 +1,8 @@
+// LANGUAGE: Java
+// AUTHOR: Ishu Raj
+// GITHUB: https://github.com/RajIshu
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
