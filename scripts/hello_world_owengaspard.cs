09bb476bcc9be14c5a06a495277c973485e416c5
Owen Gaspard <owen@ogaspard.com>
2022-10-09 17:27:34 -0500

    Create hello_world_owengaspard.cs

---
+++ scripts/hello_world_owengaspard.cs
@@ -0,0 +1,13 @@
+// LANGUAGE: C#
+// AUTHOR: Owen Gaspard
+// GITHUB: https://github.com/owengaspard
+
+namespace hello_world_owengaspard
+{
+    class hacktoberfest {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
