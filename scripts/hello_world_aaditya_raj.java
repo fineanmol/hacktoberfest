734695fe3502258b0f64074ec45e09c668e7a272
Aaaditya1612 <aadityaraj085@gmail.com>
2022-10-11 22:28:39 +0530

    added-hello-world-script

---
+++ scripts/hello_world_aaditya_raj.java
@@ -0,0 +1,12 @@
+// LANGUAGE: Java
+// AUTHOR: Aaditya Raj
+// GITHUB: https://github.com/aaditya1612
+
+import java.io.*;
+
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
