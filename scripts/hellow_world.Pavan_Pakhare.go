b35c316ffd7ba10306a3c448fccb034d97903f99
Pavan <60210984+pavanpakhare@users.noreply.github.com>
2022-10-11 11:09:42 +0530

    Add files via upload

---
+++ scripts/hellow_world.Pavan_Pakhare.go
@@ -0,0 +1,11 @@
+// LANGUAGE: Golang
+// ENV: GO
+// AUTHOR: Pavan-Pakhare
+// GITHUB: https://github.com/pavanpakhare
+
+package main
+import "fmt"
+func main() {
+
+	fmt.Println("!... Hello World ...!")
+}
