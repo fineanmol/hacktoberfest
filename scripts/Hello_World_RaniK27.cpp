5bf94ede6986a9af584a55343c333ed345438550
Reet Khare <rani11rk@gmail.com>
2022-10-12 22:06:36 +0530

    Added RaniK27 Script

---
+++ scripts/Hello_World_RaniK27.cpp
@@ -0,0 +1,14 @@
+// Language - Cpp
+// Author - Reet
+// Github - https://github.com/RaniK27
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"Hello World!";
+   cout<<"Hacktoberfest 2022 ";
+   return 0;
+}
