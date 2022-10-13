88f019f06328bf3cbc7bdfb2c27040135462cc0f
Pranotii <pranoti0820@gmail.com>
2022-10-13 17:30:03 +0530

    commit

--- scripts/hello.java
+++ scripts/hello.java
@@ -5,7 +5,7 @@ package holaMundo;
 // AUTHOR: Pranoti Durugkar
 // GITHUB: https://github.com/PranotiPD
 
-//console.log('Hello, World!');
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
