3d1e890cd13d99134ae95aee550ade1c5494a3b4
Rohit Vyas <navylover2916@gmail.com>
2022-10-14 10:06:38 +0530

    Create hello_world.cpp

---
+++ scripts/hello_world.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: CPP
+// AUTHOR: Rohit Vyas
+// GITHUB: https://github.com/Draconus524
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
