ed4f8f0f4c4c236ebd98379c379ec6d2d3a5e8a0
VENNELA2132 <61844572+VENNELA2132@users.noreply.github.com>
2022-10-15 22:40:56 +0530

    Create hello_world_vennela2132.cpp

---
+++ scripts/hello_world_vennela2132.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Erni Sri Satya Vennela
+// GITHUB: https://github.com/vennela2132
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout<<"HELLO WORLD!!";
+  return 0;
+}
