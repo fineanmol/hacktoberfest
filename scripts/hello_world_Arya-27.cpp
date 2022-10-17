4c0871ad2da2859e067353fd78b558fa97c538b2
Arya <mishraarya528@gmail.com>
2022-10-17 10:13:09 +0530

    Added hello world in c++

---
+++ scripts/hello_world_Arya-27.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// AUTHOR: Arya Mishra
+// GITHUB: https://github.com/Arya-27
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World!";
+    return 0;
+}
