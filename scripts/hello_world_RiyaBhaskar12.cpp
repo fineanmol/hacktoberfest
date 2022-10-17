b035b456c1f1a6f5f8cfa7c793cf3cf08932c677
Riya Bhaskar <riyabhaskar2@gmail.com>
2022-10-17 11:27:32 +0530

    RiyaBhaskar12

---
+++ scripts/hello_world_RiyaBhaskar12.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Riya Bhaskar
+// GITHUB: https://github.com/RiyaBhaskar12
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  cout << "I'm Rhythm Bhatia";
+  
+  return 0;
