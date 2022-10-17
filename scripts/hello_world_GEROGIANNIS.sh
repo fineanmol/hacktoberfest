1dc390c23bf9092ddeca25f50bb5d96f6d392901
Gerogiannis <54914456+GEROGIANNIS@users.noreply.github.com>
2022-10-17 20:37:32 +0300

    Create hello_world_GEROGIANNIS.sh

---
+++ scripts/hello_world_GEROGIANNIS.sh
@@ -0,0 +1,10 @@
+# LANGUAGE: BASH
+# ENV: Bash
+# AUTHOR: GEROGIANNIS
+# GITHUB: https://github.com/GEROGIANNIS
+
+
+
+#!/bin/sh
+# A Simple Bash Program To Print "Hello World"
+echo " Hello World "
