739a1ddfbeef932a62739f64d0fad4b055aaadf4
rodrigo souza <rodrigo.sistemas.infor@gmail.com>
2022-10-17 22:21:49 -0300

    [FEAT] - Add new file

---
+++ scripts/hello_word_rodrigo_1.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: José Rodrigo Guimarães de Souza
+// GITHUB: https://github.com/rodrigosistemasinfor
+
+console.log('Hello, World!');
+console.log('Hello, Hacktoberfest 2022!');
