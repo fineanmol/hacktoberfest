4392fff18b8176f003b43ec289e2aef69ad130b7
fulpopy <82092014+fulpopy@users.noreply.github.com>
2022-10-18 22:30:32 +0530

    Create hello_world_akash.cpp

---
+++ scripts/hello_world_akash.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Akash Deep Singh
+// GITHUB: https://github.com/fulpopy
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hello, World!";
+    return 0;
+}
