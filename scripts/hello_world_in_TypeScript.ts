88adde76942bc7e27eb4ed975eb7c6e749eb2e51
Luiz Felipe <luizfelper@gmail.com>
2022-10-22 11:15:20 -0300

    My First Open Source PR

---
+++ scripts/hello_world_in_TypeScript.ts
@@ -0,0 +1,8 @@
+// LANGUAGE: TypeScript
+// ENV: Node.js
+// AUTHOR: Felipe Fontenele
+// GITHUB: https://github.com/luizfelper
+
+const helloWorld: string = "Wello World";
+
+console.log(helloWorld);
