9c99da2ee8c954f5e90d553ea193f35160e4112d
Niranjan Bhoi <59923660+niranjanbhoi@users.noreply.github.com>
2022-10-26 01:39:36 +0530

    Create hello_world_niranjanbhoi.java

---
+++ scripts/hello_world_niranjanbhoi.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// ENV: VS code + jdk
+// AUTHOR: Niranjan Bhoi
+// GITHUB: https://github.com/niranjanbhoi
+class hello_world_niranjanbhoi {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
