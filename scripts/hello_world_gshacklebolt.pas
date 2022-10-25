ff4b22c06c2ab344380682f5af7b9902a6fc901b
GShacklebolt <ghazali123456@gmail.com>
2022-10-25 16:46:01 +0530

    hello world in Pascal

---
+++ scripts/hello_world_gshacklebolt.pas
@@ -0,0 +1,11 @@
+{
+ LANGUAGE: Pascal
+ ENV: Free Pascal Compiler version 3.0.2
+ AUTHOR: Ghazali
+ GITHUB: https://github.com/gshacklebolt
+}
+
+Program HelloWorld(output);
+begin
+  writeln('Hello, World!');
+end.
