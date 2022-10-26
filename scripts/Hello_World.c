e8e3477cb941191ff727ec85f4449501b66e3000
Ebin <79463025+ebinvarghese020@users.noreply.github.com>
2022-10-26 20:12:23 +0530

    Create Hello_World.c

---
+++ scripts/Hello_World.c
@@ -0,0 +1,11 @@
+// Language: C
+// Contributor: EBIN
+// Github profile link: (https://github.com/ebinvarghese020)
+
+
+#include <stdio.h>
+void main()
+{
+    printf("\nHello World!\n");
+    printf("Nice to be here \n");
+}
