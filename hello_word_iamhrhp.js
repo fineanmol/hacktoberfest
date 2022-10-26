0b697b794accb859232f00736b12a2503f2932d7
iamhrhp <hrhp007@gmail.com>
2022-10-27 02:11:57 +0530

    'branch-new'

---
+++ hello_word_iamhrhp.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Parvej khan
+// GITHUB: https://github.com/iamhrhp
+
+console.log('Hello, World!');
+console.log('Hello, Open Source!');
+console.log('Hello, Hacktoberfest!');
