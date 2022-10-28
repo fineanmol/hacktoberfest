cd52f910753a1522fcc98864124f0de4e1a18e24
MD RASHID ALI <rashid.zeyad2u@gmail.com>
2022-10-28 23:59:03 +0530

    added script file hello_world_RAshidAZ.js

---
+++ scripts/hello_world_RAshidAZ.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Md Rashid Ali
+// GITHUB: https://github.com/RAshidAZ
+// LEARN JAVASCRIPT: https://javascript.info/
+
+console.log('Hello, World!');
