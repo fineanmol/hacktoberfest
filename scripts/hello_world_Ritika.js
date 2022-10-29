0806290c8989cdb89843d1ce88477673864d6c3f
Ritika Rathore <er.ritikarathore@gmail.com>
2022-10-29 23:00:33 +0530

    This is my first contribution

---
+++ scripts/hello_world_Ritika.js
@@ -0,0 +1,12 @@
+// LANGUAGE: Javascript
+// ENV: VS code
+// AUTHOR: Ritika Rathore
+// GITHUB: https://github.com/RitikaRathore05
+
+function printHello() {
+    console.log("This is Ritika Rathore");
+    console.log("Hello world!!!");
+    console.log("Hello, Hacktoberfest!!!");
+  }
+
+  printHello();
