3acc6fd382231762d8b675c78cec02a439dad317
Siddhant109 <84970007+Siddhant109@users.noreply.github.com>
2022-10-29 12:23:56 +0530

    Create Hello_World_Siddhant.cpp

---
+++ scripts/Hello_World_Siddhant.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Siddhant Sontakke
+// GITHUB: https://github.com/Siddhant109
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World";
+	return 0;
+}
