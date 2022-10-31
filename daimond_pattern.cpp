d4e7047e3251086fa66c08d54d9ce4bec64228d6
siddhi rawool <rawoolsiddhi814@gmail.com>
2022-10-31 11:10:47 +0530

    Create daimond_pattern.cpp

---
+++ daimond_pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ 
+int n, s, i, j;
+cout << "Enter number of rows: ";
+cin >> n;
+
+for(i = 0; i <= n; i++) {
+       for(s = n; s > i; s--)
+            cout << " ";
+       for(j=0; j<i; j++)
+           cout << " * ";
+           cout << "\n";
+ }
+for(i = 1; i < n; i++) {
+       for(s = 0; s < i; s++)
+             cout << " ";
+       for(j = n; j > i; j--)
+              cout << "* ";
+
+              cout << "\n";
+}
+return 0;
+}
