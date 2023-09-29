af7c7033584db682a03d7ff02494e4faea0e39b0
Kartikey Bhatnagar <kartikey.bhatnagar2021@vitstudent.ac.in>
2023-09-29 17:33:01 +0530

    Create hello_world_kartikey-codes.js

---
+++ scripts/hello_world_kartikey-codes.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Kartikey Bhatnagar
+// GITHUB: https://github.com/kartikey-codes
+
+console.log('Hello, World!');
+console.log('for hacktober 2023');
