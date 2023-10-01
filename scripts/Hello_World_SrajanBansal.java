d8522986e1707ad857902700965bc0485ee30d3c
Srajan Bansal <srajanbansal999@gmail.com>
2023-10-01 12:33:05 +0530

    Commit message

---
+++ scripts/Hello_World_SrajanBansal.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: SRAJAN BANSAL
+// GITHUB: https://github.com/Srajan-Bansal
+
+class Hello_World_SrajanBansal {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
