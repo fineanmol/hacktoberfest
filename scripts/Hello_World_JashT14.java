4fdadd9c3b9dd50e5a1effe91c56ae57af2ceb5d
Jash Thakkar <86123342+JashT14@users.noreply.github.com>
2023-10-02 17:55:57 +0530

    All Tasks Completed
    Created a Java Hello World program

---
+++ scripts/Hello_World_JashT14.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Jash T
+// GITHUB: https://github.com/JashT14
+
+public class HelloWorld{
+ public static void main(String args){
+  System.out.println("Hello, World");
+ }
+}
