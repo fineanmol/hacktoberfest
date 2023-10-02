2327906d5ebf7e5b556f6448a6eba1a416654451
Sushruth <ssushruth2003@gmail.com>
2023-10-01 21:26:55 -0700

    hello script

---
+++ scripts/hello_world_sushruth.cpp
@@ -0,0 +1,9 @@
+// Name : Sushruth 
+// Github Profile : https://github.com/SushruthRao
+
+#include<iostream.h>
+using namespace std;
+int main(){
+cout << "Hello from Sushruth !" << endl;
+return 1;
+}
