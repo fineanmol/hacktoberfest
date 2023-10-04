57c367df80105fbc13f87bed00cd73eed68d9f15
Harsh Srivastava <92949812+hrs4real@users.noreply.github.com>
2023-10-05 01:37:36 +0530

    Create HELLO_WORLD_HARSH.sh

---
+++ scripts/HELLO_WORLD_HARSH.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This is a comment!
+echo "Hello World"
+# after save the code use chmod 755 file_name.
+#insead of 755, you can use +x means to give permission for execution.
+#or you can directly use bash file_name
