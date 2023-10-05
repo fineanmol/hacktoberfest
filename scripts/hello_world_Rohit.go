a76b85bd899f30a03ebdc96853cf0f82873ec4a2
Rohit Kumar <urskartik9@gmail.com>
2023-10-05 22:19:49 +0530

    initial commit

---
+++ scripts/hello_world_Rohit.go
@@ -0,0 +1,12 @@
+// LANGUAGE: Golang
+// ENV: go1.18.1
+// AUTHOR: RRohit Kartik
+// GITHUB: https://github.com/rkartik9
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
