292b34e6f4ae1fd0d1ad21867a7667274ab3b9b7
Harsh76200 <harshjain76200@gmail.com>
2023-10-05 10:36:46 -0700

    Modified contributors.md

--- scripts/.vscode/hello.java
+++ scripts/.vscode/hello.java
@@ -2,8 +2,8 @@
 // AUTHOR: Harsh
 // GITHUB: https://github.com/Harsh76200
 
-public class hello {
+public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println(" Hello World! ");
     }
 }
