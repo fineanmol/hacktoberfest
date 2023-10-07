a56a9b171ebf76a57653ab4c4bf4ecf070399cfe
AbhishekPaul08 <asp0078055@gmail.com>
2023-10-07 22:51:47 +0530

    added

---
+++ scripts/hello_world_ABHISHEKPAUL.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+printf("Hello, World!"); 
+return 0 //int type should always return something. Hence we return 0
+}
---
+++ scripts/hello_world_AbhishekPaulP.py
@@ -0,0 +1,1 @@
+print("Hello, World!") #that's it simple.
