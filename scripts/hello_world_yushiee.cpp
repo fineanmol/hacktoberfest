3e60d80fbddefb5fbb918dc0fdac3d08291eaf56
Ayushi <ghghgh.com>
2023-10-08 12:45:32 +0530

    Made my first contribution

---
+++ scripts/hello_world_yushiee.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Ayushu
+// GITHUB: https://github.com/yushiee
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<< "Hello from the other s-i-d-e!";
+    return 0;
+}
