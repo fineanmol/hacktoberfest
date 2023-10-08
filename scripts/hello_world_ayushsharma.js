aa78ae390936b5eafcf4ba25b5450e4c59f8cd4d
enayush <en.ayushshr@gmail.com>
2023-10-08 13:00:33 +0530

    Upadted contributing.md file and added files in profiles and scripts

---
+++ scripts/hello_world_ayushsharma.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Alysson Mascarenhas
+// GITHUB: https://github.com/AlyssonMascarenhas
+
+console.log('Hello World!');
+console.log('Ayush Sharma this side');
