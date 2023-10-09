f8acc9d4ed037bf93b5bb0b4e60406cbe4264e30
Ankit Saurabh <ankitsaurabh57@gmail.com>
2023-10-09 10:24:30 +0530

    Ankit

--- scripts/hello_world_ankits57.js
+++ scripts/hello_world_ankits57.js
@@ -1,3 +1,8 @@
+//Language:Javascript
+//ENV: node.js
+//Author: Ankit Saurabh 
+//Github: https://github.com/ankits57
+
 console.log('Hello, World!');
 console.log('Hello, Open Source!');
 console.log('Hello, Hacktoberfest!');
