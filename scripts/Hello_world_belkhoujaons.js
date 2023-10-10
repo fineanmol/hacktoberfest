f37903732e6285da35883789574f7ce3ad2787a1
ons.belkhouja <ons.belkhouja@proxym-it.com>
2023-10-10 12:37:07 +0100

    add myself

---
+++ scripts/Hello_world_belkhoujaons.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Ons Belkhouja
+// GITHUB: https://github.com/belkhoujaons
+
+console.log('Hello, World!');
+console.log('\x1b[31m%s\x1b[0m', 'Hello all Tunisian people here !!!!')
