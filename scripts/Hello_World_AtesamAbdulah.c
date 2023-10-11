3e91114e0f75ce3f1f9bcc7cedf06a28ab18a2a2
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-10-11 14:53:37 +0500

    Added comments to script

--- scripts/Hello_World_AtesamAbdulah.c
+++ scripts/Hello_World_AtesamAbdulah.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
+// LANGUAGE: c
+// ENV: N/A
+// AUTHOR: Atesam Abdullah
+// GITHUB: https://github.com/ATESAM-ABDULLAH
 
+#include <stdio.h>
 int main(){
     printf("Hello World!\n");
     return 0;
