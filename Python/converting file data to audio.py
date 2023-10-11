7ad7fd620276d9a47396b082b7945995f7b9f4f9
SPANDAN PADHI <123059161+spandan0724@users.noreply.github.com>
2023-10-12 02:22:42 +0530

    Update converting file data to audio.py
    1. It deals with problems that might happen in a smarter way.
    2. It saves the audio as 'audioout.mp3' instead of 'videoout.mp4.'
    3. It opens the audio file using a more flexible and secure method.
    4. It gives you messages to explain what's going on or if something goes wrong.

--- Python/converting file data to audio.py
+++ Python/converting file data to audio.py
@@ -1,8 +1,19 @@
 from gtts import gTTS
 import os
+import subprocess
 
-text=open('demogen.txt','r',encoding='utf-8').read()
-language='en'
-output=gTTS(text=text,lang=language,slow=False)
-output.save('videoout.mp4')
-os.system('start videoout.mp4')
+try:
+    text = open('demogen.txt', 'r', encoding='utf-8').read()
+    language = 'en'
+    
+    # Specify the output file format (audio)
+    output = gTTS(text=text, lang=language, slow=False)
+    output.save('audioout.mp3')
+    
+    print("Text converted to audio successfully!")
+
+    # Use subprocess to open the audio file
+    subprocess.Popen(['start', 'audioout.mp3'], shell=True)
+    
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
