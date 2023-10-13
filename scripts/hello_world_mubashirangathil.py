cdb6729eb6a4c99c225cb53ad9a18d6f834f4fa0
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-10-14 03:32:44 +0530

    Add Hello World Python script

---
+++ scripts/hello_world_mubashirangathil.py
@@ -0,0 +1,12 @@
+# ==================================== Hello World ==============================================================
+# LANGUAGE: Python
+# ENV    : Python3.5
+# AUTHOR : Mubashir Angathil
+# GITHUB : https://github.com/mubashir-angathil
+# ================================================================================================================
+
+def hello_world():
+    print("Hello World 👋")
+
+if __name__ == "__main__":
+    hello_world()
