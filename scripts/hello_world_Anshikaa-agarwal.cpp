0df8325e38198b3fbdba6a3fa8534074a74ba89c
Anshika <anshika2020ag@gmail.com>
2023-10-14 19:37:28 +0530

    task3 completed

---
+++ scripts/hello_world_Anshikaa-agarwal.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// AUTHOR: Anshika Agarwal
+// GITHUB: https://github.com/Anshikaa-agarwal
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
