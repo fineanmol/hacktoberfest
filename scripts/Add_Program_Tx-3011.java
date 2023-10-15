a1edb7f94eddb099c6bfc7bdfdc3927c0fad7e3a
Tejdeep Nookala <63906769+Tx-3011@users.noreply.github.com>
2023-10-15 13:15:01 +0530

    Create Add_Program_Tx-3011.java
    made a simple add program

---
+++ scripts/Add_Program_Tx-3011.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Gimme The First Number:");
+        int a = in.nextInt();
+        System.out.println("Gimme The Second Number");
+        int b = in.nextInt();
+        int sum = a + b;
+        System.out.println("The Sum of the numbers you gave is " + sum );
+
+
+
+
+
+    }
+}
