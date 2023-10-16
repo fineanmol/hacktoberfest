4b4c76c20e4dd45db3f323926f0b3782b663962e
Gabriel Brisolara <dev.brisolara@gmail.com>
2023-10-16 17:14:26 -0300

    added my hello world

---
+++ scripts/hello_world_GabrielBrisolara.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Gabriel Brisolara
+// GITHUB: https://github.com/brisolarag
+
+console.log('Hello, World!');
+console.log('Hello, Open Source!');
+console.log('Hello, Hacktoberfest!');
