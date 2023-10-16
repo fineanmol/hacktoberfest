3790b9ba033d0d4d86e0658de034d28de30495e2
Siddharth <114325371+siddHacks@users.noreply.github.com>
2023-10-17 01:18:11 +0530

    first time commit

---
+++ scripts/Python Program/hello_world_siddHacks.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// AUTHOR: Siddharth Panda
+// GITHUB: https://github.com/siddHacks
+
+console.log('Hello, World!');
+console.log('Hello, Open Source!');
+console.log("Hey Devs");
