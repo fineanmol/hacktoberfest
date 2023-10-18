1be31d124fb34f038d7d727105b79475cb02d23b
Aditi Chhabra <121351859+AditiChhabra@users.noreply.github.com>
2023-10-18 21:54:15 +0530

    Create hello_world_AditiChhabra.java

---
+++ scripts/hello_world_AditiChhabra.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// AUTHOR: Aditi Chhabra
+// GITHUB: https://github.com/AditiChhabra
+
+import java.util.*;
+public class HelloWorld{
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+      }
