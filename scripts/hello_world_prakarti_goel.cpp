c61ceb81b94155554246a2074aaf23a37de9c192
Prakarti Goel <prakartigoel24@gmail.com>
2023-10-20 00:31:17 +0530

    Added hello world script in C++

---
+++ scripts/hello_world_prakarti_goel.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Prakarti Goel
+// GITHUB: https://github.com/prakartigoel24
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World"<<endl;
+    cout << "Happy Hacktoberfest!!!";
+	return 0;
+}
