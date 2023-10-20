6f0744910036c09e1bfbcb5de8cee6962efa8438
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-10-20 11:11:04 -0300

    feat: add hello world in php.

--- scripts/hello-world-php.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$greeting = 'Hello, World!';
-echo $greeting;
---
+++ scripts/hello_world_php_yanpenalva.php
@@ -0,0 +1,9 @@
+// LANGUAGE: PHP
+// ENV: PHP
+// AUTHOR: Yan Penalva
+// GITHUB: https://github.com/yanbrasiliano
+
+<?php
+
+$greeting = 'Hello, World!';
+echo $greeting;
