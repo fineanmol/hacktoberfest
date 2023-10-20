b51b70077c14bb8b2eec8fdf8062f9465d3e03b4
SrSurajithPranav <91361858+SrSurajithPranav@users.noreply.github.com>
2023-10-20 16:52:27 +0530

    Update converting file data to audio.py

--- Python/converting file data to audio.py
+++ Python/converting file data to audio.py
@@ -1,14 +1,11 @@
 from gtts import gTTS
 import os
-
 input_file = 'demogen.txt'
 language = 'en'
 output_file = 'videoout.mp4'
-
 try:
     with open(input_file, 'r', encoding='utf-8') as file:
         text = file.read()
-
     output = gTTS(text=text, lang=language, slow=False)
     output.save(output_file)
     os.system(f'start {output_file}')
