c29efc8c2e9cf9d2a3c95df56db3fa29f980f4ff
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-10-20 11:20:05 -0300

    feat: add hello world in go.

---
+++ scripts/hello_world_go_yanpenalva.go
@@ -0,0 +1,12 @@
+// LANGUAGE: GO
+// ENV: GO
+// AUTHOR: Yan Penalva
+// GITHUB: https://github.com/yanbrasiliano
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
