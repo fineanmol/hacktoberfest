0c74761f7fca218a1c5bf540b84e3dd376e9d1ed
12345rams <132057037+12345rams@users.noreply.github.com>
2023-10-21 17:46:45 +0530

    Create HellowWorldRamvijay.java

---
+++ scripts/HellowWorldRamvijay.java
@@ -0,0 +1,8 @@
+// LANGUAGE: Java
+// AUTHOR: Ram Vijay Yadav
+// GITHUB: https://github.com/12345rams
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
