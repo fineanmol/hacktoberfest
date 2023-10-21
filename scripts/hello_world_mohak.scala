d790ded68994272be742739463ef3c69c2605dc0
Mohak Purwar <mohakpurwar72@gmail.com>
2023-10-21 20:35:21 +0530

    hacktoberfest-accepted

---
+++ scripts/hello_world_mohak.scala
@@ -0,0 +1,10 @@
+#  LANGUAGE: Scala
+#  ENV: Scala
+#  AUTHOR: mohak
+#  GITHUB: https://github.com/mohakpurwar
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+}
