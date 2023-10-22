047c7832b555ea8db9da8116c17a3c3ba6c23785
Sakshi2518 <vk18sakshi@gmail.com>
2023-10-22 14:22:38 +0530

    first open source contribution

---
+++ profiles/sakshi.md
@@ -0,0 +1,17 @@
+# Sakshi Singh
+
+### Location
+
+New Delhi
+
+### Academics
+
+Sophomore at IGDTUW
+
+### Interests
+
+
+
+### Profile Link
+
+[Sakshi Singh](https://github.com/Sakshi2518)
---
+++ scripts/hello_world_sak2518.java
@@ -0,0 +1,5 @@
+public class hello_world_sak2518 { //class name should be the same as the name of the file
+  public static void main(String[] args) {
+    System.out.println("Hello World"); //write the content you want to display on the screen insdie the print statement
+  }
+}
