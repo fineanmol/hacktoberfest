8e7da53ddddfaa631f09a70761041567da711ed7
Nitin Shukla <143546627+Nitinshukla88@users.noreply.github.com>
2023-10-23 10:33:13 +0530

    Done all the 3 tasks

---
+++ scripts/hello_world_nitin_shukla.go
@@ -0,0 +1,12 @@
+// LANGUAGE: Golang
+// ENV: GO
+// AUTHOR: nitin shukla
+// GITHUB: https://github.com/Nitinshukla88
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
