10ad2f2bd77f4a3a9188e68c09ab0edd75e6c721
ShlokChaitanya <shlokchaitanya120@gmail.com>
2023-10-24 13:35:50 +0530

    Commit message

---
+++ scripts/hello_world_shlokchaitanya.java
@@ -0,0 +1,23 @@
+public class hello_world_shlokchaitanya {
+    public static void main(String[] args) {
+        String h = "hello world";
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        String s = "";
+        for (int i = 0; i < h.length(); i++) {
+            for (int j = 0; j <= abc.length(); j++) {
+                if (h.charAt(i) == ' ') {
+                    System.out.print(" ");
+                    s = s + " ";
+                    break;
+                }
+                if (abc.charAt(j) == h.charAt(i) || abc.charAt(j) != h.charAt(i)) {
+                    System.out.println(s + abc.charAt(j));
+                    if (abc.charAt(j) == h.charAt(i)) {
+                        s = s + abc.charAt(j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
