c0545ac0659f69035189a124c54ca0d6db32061a
Jay <jaygondaliya2309@gmail.com>
2024-10-02 23:13:57 +0530

    Commit changes

---
+++ scripts/hello_world_jayg2309.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// AUTHOR: Jay Gondaliya
+// GITHUB: https://github.com/jayg2309 
+
+public class hello_world_jayg2309
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
