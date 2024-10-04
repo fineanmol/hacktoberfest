b2c63e8a6898603cc264b5f1d6bab20f9197ad27
BABEL-SI\alberto.rsantana <alberto.rsantana@babelgroup.com>
2024-10-04 12:00:46 +0200

    modified java file

--- scripts/Hello_world_albertorsn.java
+++ scripts/Hello_world_albertorsn.java
@@ -4,7 +4,7 @@ public class Hello_Hub_dojetin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World! from AlbertoRSN");
 	}
 
 }
