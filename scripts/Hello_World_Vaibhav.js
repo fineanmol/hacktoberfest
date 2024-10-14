de9c434dc757bb8217092c6cdf0998ceb932d0dd
vaibhav_07 <vaibhavsrivastava0710>
2024-10-14 23:02:29 +0530

    first contribution

---
+++ scripts/Hello_World_Vaibhav.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// AUTHOR: Vaibhav
+// GITHUB: https://github.com/VaibhavS0710
+
+console.log("Hello World");
+console.log("The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt");
+console.log("Hello Hacktoberfest");
