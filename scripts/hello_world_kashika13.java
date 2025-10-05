6de0e42578709ad3f4ea050e177212ab2fb2c61a
Kashika <k.kashika13+github@gmail.com>
2025-10-05 18:07:04 +0530

    Created Hello World Script in Java

---
+++ scripts/hello_world_kashika13.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: Javac
+// AUTHOR: Kashika Kushwaha
+// GITHUB: https://github.com/kashika13
+
+public class hello_world_kashika13 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
