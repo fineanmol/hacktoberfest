dc8a9787711ed320f5f3ed00bcd1311f6819aed0
Shubham <shubhammittal2241@gmail.com>
2025-10-06 20:44:53 +0530

    changed

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,36 +1,44 @@
+a1 = b1 = 0
 
-a1=b1=0
 def game():
-    a=input("Player 1 enter rock,paper or scissor: ")
-    b=input("Player 2 enter rock,paper or scissor: ")
+    a = input("Player 1 enter rock, paper or scissor: ").strip().lower()
+    b = input("Player 2 enter rock, paper or scissor: ").strip().lower()
     global a1
     global b1
-    if a==b:
+
+    if a == b:
         print("This round is draw")
-        a1+=1
-        b1+=1
+        a1 += 1
+        b1 += 1
     else:
-        if a=="rock" and (b=="scissor" or b=="paper"):
-            a1+=1
+        if a == "rock" and b == "scissor":
+            a1 += 1
             print("Player 1 wins this round")
 
-        elif a=="scissor" and b=="rock":
-            b1+=1
+        elif a == "scissor" and b == "rock":
+            b1 += 1
             print("Player 2 wins this round")
-        elif a=="scissor" and b=="paper":
-            a1+=1
-            print("Player 1 wins this round")
-        elif a=="paper" and (b=="rock" or b=="scissor"):
-            b1+=1
-            print("Player 2 wins this round")
-game()
-game()
-game()
-if a1==b1:
-    print("It's an overall draw: Score: ",end="")
 
-elif a1>b1:
-    print("Player 1 won: Score: ",end="")
+        elif a == "scissor" and b == "paper":
+            a1 += 1
+            print("Player 1 wins this round")
+
+        elif a == "paper" and b == "rock":
+            a1 += 1
+            print("Player 1 wins this round")
+
+        elif a == "paper" and b == "scissor":
+            b1 += 1
+            print("Player 2 wins this round")
+
+for i in range(3):
+    game()
+
+if a1 == b1:
+    print("It's an overall draw: Score: ", end="")
+elif a1 > b1:
+    print("Player 1 won: Score: ", end="")
 else:
-    print("Player 2 won: Score: ",end="")
-print(a1,b1,end="-")
+    print("Player 2 won: Score: ", end="")
+
+print(a1, b1, end="-")
