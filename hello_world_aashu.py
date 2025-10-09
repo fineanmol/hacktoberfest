f6244280be50b3e39a36c427b941d2ce6867f7a2
root <root@noob.localdomain>
2025-10-09 19:26:12 +0530

    Fix file name and add header for Hacktoberfest validation

---
+++ hello_world_aashu.py
@@ -0,0 +1,12 @@
+# LANGUAGE: Python
+# ENV: Any
+# AUTHOR: Akshat Patil
+# GITHUB: https://github.com/aashu2006
+
+num = int(input("Enter a number: "))
+
+if str(num) == str(num)[::-1]:
+    print(f"{num} is a palindrome!")
+else:
+    print(f"{num} is not a palindrome!")
+
