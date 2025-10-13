f138408c3dbea3219a020eb495a72a35412c35e1
ti-v <thetivishasinha@gmail.com>
2025-10-12 19:30:06 -0700

    Add files via upload

---
+++ scripts/Hello_World_tiv.py
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
+{\colortbl;\red255\green255\blue255;\red236\green244\blue251;\red12\green14\blue18;}
+{\*\expandedcolortbl;;\cssrgb\c94118\c96471\c98824;\cssrgb\c5098\c6667\c9020;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 # LANGUAGE: Python\
+# ENV: Python 3.x\
+# AUTHOR: Tivisha Sinha\
+# GITHUB: https://github.com/ti-v\
+\
+print("Hello, World!")\cf2 \cb1 \strokec2 \
+}
