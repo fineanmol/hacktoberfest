338361f18e00ca9ec2fa4ac98a45a5cb80e5e86d
Akank Pattnaik <96375225+Sonu-3@users.noreply.github.com>
2025-10-14 16:09:16 +0530

    Update hello_word_sonu-3.py

--- scripts/hello_word_sonu-3.py
+++ scripts/hello_word_sonu-3.py
@@ -1,4 +1,8 @@
+# LANGUAGE: Python
+# AUTHOR: Akank Pattnaik
+# GITHUB: https://github.com/Sonu-3
+
 var = "Namaste Duniya"
 en_var = "Hello World"
 
-print(f"Hindi:{var}/n English:{en_var}")
+print(f"Hindi:{var}/nEnglish:{en_var}")
