e60d9e335b7d20dbc617a72e07e3522ddc4bdb33
haris-bit <harisiftikhar109@gmail.com>
2025-10-14 20:31:17 -0500

    added contributions for hacktoberfest 2025

---
+++ scripts/hello_world_haris.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: Javac
+// AUTHOR: Haris Iftikhar
+// GITHUB: https://github.com/haris-bit
+
+public class hello_world_haris {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
