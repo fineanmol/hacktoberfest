6daf042cb710910723388f3dd0efd75eef5150bd
sojwalorison-hub <sojwal.orison@gmail.com>
2025-10-20 00:20:01 +0530

    Create Pallindrome.java

---
+++ Pallindrome.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+
+    String str = "Radar", reverseStr = "";
+    
+    int strLength = str.length();
+
+    for (int i = (strLength - 1); i >=0; --i) {
+      reverseStr = reverseStr + str.charAt(i);
+    }
+
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
+    }
+  }
+}
