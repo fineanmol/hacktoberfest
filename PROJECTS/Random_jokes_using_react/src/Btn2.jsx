bb1c8f45f00ff89506eaff6d1259b3c0bebf0199
sai <vishwajitdeshmane2004@gmail.com>
2025-10-20 11:18:51 +0530

    Added_random_jokes_generator

---
+++ PROJECTS/Random_jokes_using_react/src/Btn2.jsx
@@ -0,0 +1,4 @@
+export default function Btn2({action}){
+    return <span><button onClick={action}>Neutral</button></span>
+
+}
---
+++ PROJECTS/Random_jokes_using_react/src/Result.jsx
@@ -0,0 +1,3 @@
+export default function Result({isWinning}){
+  return <h3>{isWinning && "Congratulations you won a Lottery !"}</h3>
+}
