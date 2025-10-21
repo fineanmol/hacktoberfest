1254ad74be42479321eeb0a13ec6849cf906ae6e
Akash Keote <keoteakash@gmail.com>
2025-10-21 14:58:45 +0530

    Add basic Flutter app with text widget

---
+++ DartCode x Flutter/textwidget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 86, 16, 207)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("text in flutter"),
+      ),
+      body: Text(
+        "Text Widget Study in Flutter",
+        style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.w900,
+            color: Color.fromARGB(223, 219, 163, 9),
+            backgroundColor: Color.fromARGB(200, 17, 64, 124)),
+      ),
+    );
+  }
+}
