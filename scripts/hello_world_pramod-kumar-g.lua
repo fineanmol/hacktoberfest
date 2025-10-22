5214f533985c7cbe0e61133aa4065c1545d5f87f
Pramod Kumar <pramodg2002@gmail.com>
2025-10-22 12:39:53 +0530

    add hello world script in lua

---
+++ scripts/hello_world_pramod-kumar-g.lua
@@ -0,0 +1,8 @@
+-- LANGUAGE: Lua
+-- ENV: Lua 5.4+
+-- AUTHOR: Pramod Kumar G
+-- GITHUB: https://github.com/Pramod-Kumar-G
+
+-- A good place to learn lua is from the official docs - https://www.lua.org/pil/1.html
+
+print("Hello, World!")
