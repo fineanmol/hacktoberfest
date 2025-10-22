4475ac03c92509a292fe8ef5d5d187df01cdf6ae
Rana Shivang Singh <ranashivang567@gmail.com>
2025-10-22 23:17:06 +0530

    Added profile, name, and hello world script for Hacktoberfest

---
+++ scripts/hello_world_singhranashivang.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Rana Shivang Singh
+// GITHUB: https://github.com/Singhranashivang
+
+console.log("Hello, World! I'm contributing to Hacktoberfest 🎉");
