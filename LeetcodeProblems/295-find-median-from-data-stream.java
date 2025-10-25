aa5d70b136543da939b7f0142bab80bdd38e2a7b
Rony-2004 <mowazzemahmed19@gmail.com>
2025-10-25 23:26:38 +0530

    Add profile for ÍMowazzem Uddin Ahmed and implement MedianFinder class

---
+++ LeetcodeProblems/295-find-median-from-data-stream.java
@@ -0,0 +1,23 @@
+class MedianFinder {
+  public void addNum(int num) {
+    if (maxHeap.isEmpty() || num <= maxHeap.peek())
+      maxHeap.offer(num);
+    else
+      minHeap.offer(num);
+
+    
+    if (maxHeap.size() < minHeap.size())
+      maxHeap.offer(minHeap.poll());
+    else if (maxHeap.size() - minHeap.size() > 1)
+      minHeap.offer(maxHeap.poll());
+  }
+
+  public double findMedian() {
+    if (maxHeap.size() == minHeap.size())
+      return (double) (maxHeap.peek() + minHeap.peek()) / 2.0;
+    return (double) maxHeap.peek();
+  }
+
+  private Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+  private Queue<Integer> minHeap = new PriorityQueue<>();
+}
