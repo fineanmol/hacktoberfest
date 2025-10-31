53763c5667ab9788b28e990a406d2b0837ae12bd
Ooreoluwa <goodnessoluseyi@gmail.com>
2025-10-31 20:22:24 +0100

    Fix: Correct filename and add missing headers

---
+++ scripts/hello_world_Oore2006.py
@@ -0,0 +1,11 @@
+# LANGUAGE: Python
+# ENV: Python 3.x
+# AUTHOR: Goodness Oluseyi
+# GITHUB: https://github.com/Oore2006
+
+
+#print hello world 
+print("Hello World")
+
+#print happy hacktoberfest message
+print("Happy Hacktoberfest 2025!")
