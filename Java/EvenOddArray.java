156c29933d389fb9af9bb0c970168b48e4d89f10
Mandru-JashwanthKumar <mandrujashwanth439@gmail.com>
2026-01-01 21:20:52 +0530

    Program to Count Even and Odd Numbers in an Array Using Java
    This Java program takes the size of an array and its elements as input from the user. It then checks each element in the array to determine whether it is even or odd. Finally, the program displays the total count of even and odd numbers present in the array. The program uses basic control structures and the Scanner class for user input, making it simple and easy to understand.

--- Java/EvenOddArray.java
+++ Java/EvenOddArray.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
+
 public class EvenOddArray {
-public static void main (String[] args)
-{
-int n;
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the size of the array: ");
-n = sc.nextInt();
-int[]arr = new int[n];
-System.out.println("Enter the array elements: ");
-for(int i = 0; i < n; i++)
-{
-arr[i] = sc.nextInt();
-}
-int even = 0, odd = 0;
-for(int i=0; i<n; i++){
-if((arr[i] % 2 )== 0)
-even += 1;
-else
-odd += 1;
-}
-System.out.println("Number of even elements: "+ even);
-System.out.println("Number of odd elements: "+ odd);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int even = 0, odd = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Number of even elements: " + even);
+        System.out.println("Number of odd elements: " + odd);
+
+        sc.close();
+    }
 }
