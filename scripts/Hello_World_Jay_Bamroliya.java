6080321ca7ce996e10279ac49ec23e1e755c6c63
Jay <jaybamroliya05@gmail.com>
2026-06-05 01:10:56 +0200

    Add Profile and Program (#2660)

---
+++ scripts/Hello_World_Jay_Bamroliya.java
@@ -0,0 +1,16 @@
+
+// LANGUAGE: Java
+// AUTHOR: Jay Bamroliya
+// GITHUB: https://github.com/jaybamroliya
+
+
+public class Hello_World_Jay_Bamroliya {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
