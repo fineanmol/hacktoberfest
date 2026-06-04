073e97867c2740caa0194e455154981228d6f036
Anmol Agarwal <24794539+fineanmol@users.noreply.github.com>
2026-06-05 00:09:17 +0200

    Merge PR #3140: add macb's script
    Integrate contributor changes on master (2024 batch).

---
+++ scripts/hello_word_macb_clarke.rs
@@ -0,0 +1,9 @@
+// LANGUAGE: Rust
+// ENV: rustc
+// AUTHOR: MacbClarke
+// GITHUB: https://github.com/MacbClarke
+
+fn main() {
+    println!("Hello World!");
+    println!("Hello Hacktoberfest!");
+}
