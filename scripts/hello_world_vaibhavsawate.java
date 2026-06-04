c0eab1a3069aeccb6726a884ef6b9b6e05e0065f
Vaibhav <vaibhavsawate3@gmail.com>
2026-06-05 00:35:10 +0200

    VaibhavS's commit (#3030)

---
+++ scripts/hello_world_vaibhavsawate.java
@@ -0,0 +1,8 @@
+// LANGUAGE: Java
+// AUTHOR: Vaibhav Sawate
+// GITHUB: https://github.com/Vaibhav-Sawate
+public class hello_world_vaibhavsawate {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
