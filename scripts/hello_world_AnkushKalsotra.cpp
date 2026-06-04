692e0ade235562d0eca1852aceeed00a37bd40ba
AnkushKalsotra <mahajanankush49@gmail.com>
2026-06-05 00:41:08 +0200

    Contributing to new branch (#2961)

---
+++ scripts/hello_world_AnkushKalsotra.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Ankush Kalsotra
+// GITHUB: https://github.com/AnkushKalsotra
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;
+}
