65c8e7426b4b5ae5dac63d33d885f8faa9db48c3
Suraj Singh <suraj.ms1@icloud.com>
2026-06-05 01:40:45 +0200

    Hello World (#2306)

--- scripts/Hello_World_ssms.js
+++
@@ -1,9 +0,0 @@
-// LANGUAGE: Javascript
-// ENV: Node.js
-// AUTHOR: Suraj Singh
-// GITHUB: https://github.com/SurajSinghM
-
-
-
-console.log('Hello World !');
-console.log('Hello Everyone! I am so excited to participate in Hacktoberfest!');
