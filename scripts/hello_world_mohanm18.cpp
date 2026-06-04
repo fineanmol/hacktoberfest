3c65633140aabdebb53c7e1e8ce4912d28abcdaa
Mohan Mondhe <mohanmondhe30@gmail.com>
2026-06-05 01:09:48 +0200

    Updated CONTRIBUTING.md (#2674)

---
+++ scripts/hello_world_mohanm18.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// AUTHOR: Mohan Mondhe
+// GITHUB: https://github.com/mohanm18
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
