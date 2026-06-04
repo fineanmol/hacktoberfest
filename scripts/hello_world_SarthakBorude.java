834e220f0745a1b027eacf791e8ed196f73e25a6
Anmol Agarwal <a.agarwal@link11.com>
2026-06-05 00:07:27 +0200

    Merge PR #3238: Added my first contribution
    Integrate contributor changes on master (2024 batch).

---
+++ scripts/hello_world_SarthakBorude.java
@@ -0,0 +1,9 @@
+/**
+ * hello_world_SarthakBorude
+ */
+public class hello_world_SarthakBorude {
+
+    public static void main(String[] args) {
+        System.out.println("hello world ! enjoing this XD");
+    }
+}
