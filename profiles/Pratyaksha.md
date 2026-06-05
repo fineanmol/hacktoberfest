68f87a1caf5bc318499904217c719fe9a5c430ee
Pratyaksha Newalkar <newalkarpratyaksha@gmail.com>
2026-06-05 03:10:04 +0200

    Added the required details (#630)

---
+++ profiles/Pratyaksha.md
@@ -0,0 +1,3 @@
+<p>Hello</p>
+<p>My name is Pratyaksha Newalkar</p>
+<p>I am second year student</p>
---
+++ scripts/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t = 1;
+  
+    while (t--)
+    {
+      cout<<"\nHello World";
+    }
+  return 0;
+}
