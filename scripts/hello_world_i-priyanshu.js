03aafa9dc48f45884674a2e2f850c97306c30bf6
i-priyanshu <i.priyanshukumar01@gmail.com>
2026-06-05 03:24:55 +0200

    HacktoberFest-2022 (#410)

---
+++ scripts/hello_world_i-priyanshu.js
@@ -0,0 +1,13 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Priyanshu Kumar
+// GITHUB: https://github.com/i-priyanshu
+
+
+let name_ = "Priyanshu";
+
+function hello() {
+    console.log("Hello world! by " + name_);
+}
+
+hello();
