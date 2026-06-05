4344d8048bb1bd23a49d4511442a72abad89630f
Ritika Rathore <er.ritikarathore@gmail.com>
2026-06-05 02:23:40 +0200

    This is my first contribution (#1835)

---
+++ scripts/hello_world_Akarshit7.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// ENV: VS code
+// AUTHOR: Akarshit
+// GITHUB: https://github.com/Akarshit7
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world"<<endl;
+    return 0;
+}
