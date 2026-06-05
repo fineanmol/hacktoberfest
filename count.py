630be3ea589a86944db4224bed0bb0450e15c2b8
roybithika18 <116145065+roybithika18@users.noreply.github.com>
2026-06-05 02:30:17 +0200

    Create count.py (#1595)

--- count.py
+++ count.py
@@ -1,9 +1,12 @@
-list1 = [ ('Cat', 'Bat'), ('Sat', 'Cat'), ('Cat', 'Bat'),
-		('Cat', 'Bat', 'Sat'), [1, 2], [1, 2, 3], [1, 2] ]
+# Python3 program to count the number of times
+# an object appears in a list using count() method
 
-# Counts the number of times 'Cat' appears in list1
-print(list1.count(('Cat', 'Bat')))
+lst = ['Cat', 'Bat', 'Sat', 'Cat', 'Mat', 'Cat', 'Sat']
 
-# Count the number of times sublist
-# '[1, 2]' appears in list1
-print(list1.count([1, 2]))
+# To get the number of occurrences
+# of each item in a list
+print ([ [l, lst.count(l)] for l in set(lst)])
+
+# To get the number of occurrences
+# of each item in a dictionary
+print (dict( (l, lst.count(l) ) for l in set(lst)))
