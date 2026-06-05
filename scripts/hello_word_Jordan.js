6605879503567ec04ccd705f04580cb87f3d0c76
Jordanjun98 <junming518@gmail.com>
2026-06-05 03:10:18 +0200

    add profile  (#1497)

---
+++ scripts/hello_word_Jordan.js
@@ -0,0 +1,9 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Akshat Agarwal
+// GITHUB: https://github.com/Akshat-z
+
+console.log('Hello world!');
+console.log('Hello Hacktoberfest !');
+console.log('i am aa tester');
+
