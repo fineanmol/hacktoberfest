d01eecf0dfdaaa0022caadf725c7389c61a2f060
Rishi Kaushal <rishi29work@gmail.com>
2026-06-05 03:04:52 +0200

    Added my name to contributors and a "Hello World" script. (#554)

---
+++ scripts/Hello_world_sagefell29.py
@@ -0,0 +1,4 @@
+# This is sagefell29's(https://github.com/sagefell29) Hello world script in python.
+print("Hello World!!!!")
+print("Python is my favorite language.")
+print("I love HacktoberFest.")
