90e32471b88085fdbab294c30be0ccf77d0e2f01
Dimas Yudha P <prawira.dimas.yudha@gmail.com>
2026-06-05 02:28:07 +0200

    Hello world rust - hacktoberfest2022 (#1756)

---
+++ scripts/hello_world_dhiemaz.rs
@@ -0,0 +1,13 @@
+// LANGUAGE: Rust
+// ENV: Rust
+// AUTHOR: Dimas Yudha Prawira
+// GITHUB: https://github.com/dhiemaz
+//
+// How to run:
+// $rustc hello_world_rust.rs
+// $./hello_world_rust
+
+
+fn main(){
+    println!("Hello, world!");
+}
